46ae6becf605ac3e853ba8befd9e5e554602b174
mateus-code-98 <teu-9@hotmail.com>
2023-03-20 00:27:14 -0300

    fix: remove transition of the button 'ScrollToTopButton'

--- src/components/ScrollToTopButton/style.ts
+++ src/components/ScrollToTopButton/style.ts
@@ -2,26 +2,23 @@ import styled, { css } from "styled-components";
 import { shadowColor, tertiary } from "../../theme";
 
 export const Container = styled.div<{ open: boolean }>`
+    cursor: pointer;
+    box-shadow:0.125rem 0.125rem 0.5rem ${shadowColor};
+    background: ${tertiary};
     position: fixed;
     opacity: 0;
+    display: none;
     justify-content: center;
     align-items: center;
     width: 40px;
     height: 40px;
-    border-radius: 50%;
     bottom: 16px;
-    display: flex;
-    background: ${tertiary};
+    border-radius: 50%;
     right: calc(50vw - 255px);
-    box-shadow:0.125rem 0.125rem 0.5rem ${shadowColor};
-    -webkit-transition: opacity 0.3s ease;
-    -moz-transition: opacity 0.3s ease;
-    -ms-transition: opacity 0.3s ease;
-    -o-transition: opacity 0.3s ease;
-    transition: opacity 0.3s ease;
-    cursor: pointer;
     ${props => props.open && css`
         opacity: 1;
+        width: 40px;
+        display: flex;
     `}
     @media (max-width: 600px) {
         right: 30px;
